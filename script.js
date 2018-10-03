37c9090c9319c2b985d717abb06b99b0f42ab80d
Luci-Lawless <lu.gmmm@gmail.com>
2018-10-04 01:31:04 +0200

    Add scroll to the top button

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+// Back to top Button
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// Scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
